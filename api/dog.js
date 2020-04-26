d00e0ffa4cbf02f35e21479f7d3a044a1240524a
Taiga Yamamoto <taiga.y1133@gmail.com>
2020-04-26 19:46:51 +0900

    NuxtでAPIを用いたギャラリーアプリ

---
+++ api/dog.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+class DogApi {
+  constructor() {
+    this.apiBase = "https://dog.ceo/api";
+  }
+
+  breeds() {
+    return axios
+      .get(`${this.apiBase}/breeds/list/all`)
+      .then(json => {
+        return json.data.message;
+      })
+      .catch(e => ({ error: e }));
+  }
+
+  dogs(breed) {
+    return axios
+      .get(`${this.apiBase}/breed/${breed}/images`)
+      .then(json => {
+        return json.data.message.map(d => {
+          return {
+            url: d,
+            like: 0
+          };
+        });
+      })
+      .catch(e => ({ error: e }));
+  }
+}
+
+const dogApi = new DogApi();
+
+export default dogApi;
